bde6d8400ad34267c3777ff6859a78c03d193430
William Stevens <contact@wastevensv.com>
2015-12-06 15:25:22 -0500

    Practicing good commenting.

--- 01-1/main.c
+++ 01-1/main.c
@@ -1,18 +1,25 @@
+/*
+ * Day 1, Challenge 1 - Parens pair counter.
+ * 2015-06-12 (December 6, 2015)
+ * William A Stevens V (wastevensv)
+ */
+
+
 #include <stdio.h>
 
 int main( int argc, char *argv[]) {
-    char    c='\0';
-    int     n=0;
+    char    c='\0'; // Current character in buffer.
+    int     n=0;    // Current floor.
     while(c != EOF) {
         c = '\0';
         n = 0;
         while(c != '\n') {
-            c = getchar();
-            if(c == '(') n++;
-            if(c == ')') n--;
-            if(c == EOF) break;
+            c = getchar(); // Get next character in buffer.
+            if(c == '(') n++; // ( - Increment floor
+            if(c == ')') n--; // ) - decrement floor
+            if(c == EOF) break; // Break on EOF.
         }
-        printf("%d\n",n);
+        printf("%d\n",n); // Print the floor at the end of each line.
     }
     return 0;
 }
--- 01-2/main.c
+++ 01-2/main.c
@@ -1,18 +1,23 @@
+/*
+ * Day 1, Challenge 2 - Parens error detector.
+ * 2015-06-12 (December 6, 2015)
+ * William A Stevens V (wastevensv)
+ */
 #include <stdio.h>
 
 int main( int argc, char *argv[]) {
-    char    c = '\0';
-    int     n = 0;
-    int     p = 1;
+    char    c = '\0';   // Current character in buffer.
+    int     n = 0;      // Floor number.
+    int     p = 1;      // Position counter.
     while(c != EOF) {
-        c = getchar();
-        if(c == '(') n++;
-        if(c == ')') n--;
-        if(n < 0) {
+        c = getchar(); // Get next character in buffer.
+        if(c == '(') n++; // ( - Increment floor
+        if(c == ')') n--; // ) - decrement floor
+        if(n < 0) { // If extra closing parens detected, output position.
             printf("%d\n",p);
             break;
         }
-        p++;
+        p++; // Increment position counter.
     }
     return 0;
 }
